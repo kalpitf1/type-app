c0d4376ce4bf19773ec654b226e703477c9dc6b0
kalpitfulwariya <kalpitf2@illinois.edu>
2021-09-26 17:53:28 +0530

    added event handler, app displays state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
+const state = { letter: '', nextLetter: '' }
+const letterArray = 'abcdefghijklmnopqrstuvwxyz'.split('')
+letterArray.push("DONE")
+
 function App() {
+  function handleChange(event) {
+    setState({ letter: event.target.value })
+    const idx = letterArray.findIndex(element => element === event.target.value)
+    const next = letterArray[idx + 1]
+    setState({ nextLetter: next })
+  }
+
   return (
     <div class="App">
       <header class="header">
@@ -16,12 +29,33 @@ function App() {
           </ul>
         </nav>
       </header>
-      <div class="main-wrapper"> App content goes here </div>
+      <div class="main-wrapper"> App content goes here
+        <p>
+          You typed: {state.letter}
+        </p>
+        <p>
+          What's next: {state.nextLetter}
+        </p>
+        <p>
+          <input onChange={handleChange} />
+        </p>
+      </div>
       <footer class="footer">
         <a href="#"> Created by @ </a>
       </footer>
     </div>
   );
 }
+
+function setState(newState) {
+  Object.assign(state, newState)
+  renderApp()
+}
+
+function renderApp() {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
 
 export default App;
