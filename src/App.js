e7b97d476779b0cdfb8bce6c27bc0faec5e0abd1
kalpitfulwariya <kalpitf2@illinois.edu>
2021-09-28 20:25:21 +0530

    added timer for keypresses

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import './App.css';
 const letterArray = 'abcdefghijklmnopqrstuvwxyz'.split('')
 letterArray.push("DONE")
 
+// timer vars
+var totalTime = 0
+var prev = 0
+
 function App() {
   const [letter, setLetter] = React.useState('')
   const [nextLetter, setNextLetter] = React.useState('a')
@@ -15,6 +19,13 @@ function App() {
       const idx = letterArray.findIndex(element => element === event.target.value)
       const next = letterArray[idx + 1]
       setNextLetter(next)
+    }
+    if (event.target.value === 'a') { // start timer when 'a' is pressed
+      prev = Date.now()
+    } else {  // add time b/w each keypress to timer
+      var elapsedTime = Date.now() - prev  // time b/w current and previous keypress
+      totalTime += elapsedTime  // increment total time
+      prev = Date.now()  // update previous keypress time
     }
     document.getElementById("letterInput").value = ''
   }
@@ -33,16 +44,17 @@ function App() {
           </ul>
         </nav>
       </header>
-      <div class="main-wrapper"> App content goes here
+      <div class="main-wrapper">
         <p>
-          You typed: {letter}
+          You typed: <strong> {letter} </strong>
         </p>
         <p>
-          What's next: {nextLetter}
+          What's next: <strong> {nextLetter} </strong>
         </p>
         <p>
           <input onChange={handleChange} id="letterInput" />
         </p>
+        <p>{totalTime ? `Total time ${(totalTime / 1000)} seconds` : 'Start typing'}</p>
       </div>
       <footer class="footer">
         <a href="#"> Created by @ </a>
